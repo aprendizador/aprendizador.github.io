012bdbc05c3e60bfcc04b68cf6ebec1cd3408ef2
aprendizador <31588837+aprendizador@users.noreply.github.com>
2017-10-17 13:01:16 -0300

    Update sw.js

--- notify/sw.js
+++ notify/sw.js
@@ -55,7 +55,7 @@ const cacheManifest = [
   "/favicon.ico"
 ];
 const NAME = 'APR';
-const VERSION = '0.1.2';
+const VERSION = '0.1.0';
 const currentCacheName = NAME + '-v' + VERSION;
 
 self.addEventListener('install', function(e) {
