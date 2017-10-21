cf86d6406b91fc902743a4d95d131205f5092b47
aprendizador <31588837+aprendizador@users.noreply.github.com>
2017-10-21 09:53:22 -0300

    Update sw.js

--- notify/sw.js
+++ notify/sw.js
@@ -104,6 +104,7 @@ self.addEventListener('fetch', function(event) {
       console.info('[SW] trabalhando em cache');
       let out = ".mdl-button--raised {background: rgba(158,158,158,.2);box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);}.mdl-button {box-sizing: border-box;align-items: flex-start;background: 0 0;border: none;border-radius: 2px;color: #000;position: relative;height: 36px;margin: 0;min-width: 64px;padding: 0 16px;display: inline-block;font-family: 'Roboto','Helvetica','Arial',sans-serif;font-size: 14px;font-weight: 500;text-transform: uppercase;letter-spacing: 0;overflow: hidden;will-change: box-shadow;transition: box-shadow .2s cubic-bezier(.4,0,1,1),background-color .2s cubic-bezier(.4,0,.2,1),color .2s cubic-bezier(.4,0,.2,1);outline: none;cursor: pointer;text-decoration: none;text-align: center;line-height: 36px;vertical-align: middle;text-shadow: none;text-indent: 0px;font: 13.3333px Arial;word-spacing: normal;text-rendering: auto;}.mdl-button{-webkit-tap-highlight-color: rgba(255,255,255,0);    }button {padding: 1px 6px;}html, body {font-family: 'Helvetica','Arial',sans-serif;font-size: 14px;font-weight: 400;line-height: 20px;margin: 0;padding: 0;}html {color: rgba(0,0,0,.87);}::selection {background: #b3d4fc;text-shadow: none;}";
       return new Response(out);
+      }));
   }
   else {
     event.respondWith(fetch(event.request).catch(function(e) {
