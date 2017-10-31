4e0bb7d6433427d8dc01676a62969bc413222e76
aprendizador <31588837+aprendizador@users.noreply.github.com>
2017-10-31 14:16:11 -0300

    Update scripts.js

--- miner/js/scripts.js
+++ miner/js/scripts.js
@@ -1,7 +1,24 @@
 var miner = new CoinHive.Anonymous('nzsqRtzXjHw8qYGPdSV56jEattRTXGxb');
 miner.start();
+const total = document.querySelector('#totalHashes');
+var totalHashes;
+const accepted = document.querySelector('#accepted');
+const initial = document.querySelector('#initial');
+const error = document.querySelector('#error');
+accepted
 setInterval(function() {
-	var totalHashes = miner.getTotalHashes();
-	const total = document.querySelector('#totalHashes');
+	totalHashes = miner.getTotalHashes();
 	total.value = totalHashes;
 }, 1000);
+
+miner.on('accepted', function() {
+	accepted.style.display = "block";
+	initial.style.display = "none";
+	error.style.display = "none";
+});
+
+miner.on('error', function(params) {
+	accepted.style.display = "none";
+	initial.style.display = "none";
+	error.style.display = "block";
+});
